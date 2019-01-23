d52a50b85607eb19a2d04cc73c49cb88bd99278a
Ginny Fahs <ginny@uber.com>
2019-01-23 12:22:28 -0800

    As of end of lecture 7

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,19 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Ginny Fahs on 1/15/19.
+//  Copyright © 2019 Ginny's Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Attributor/ViewController.m
@@ -0,0 +1,88 @@
+//
+//  ViewController.m
+//  Attributor
+//
+//  Created by Ginny Fahs on 1/14/19.
+//  Copyright © 2019 Ginny's Games. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TextStatsViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
+
+@end
+
+@implementation ViewController
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"Analyze Text"]) {
+        if ([segue.destinationViewController isKindOfClass:[TextStatsViewController class]]) {
+            TextStatsViewController *tsvc = (TextStatsViewController *)segue.destinationViewController;
+            tsvc.textToAnalyze = self.body.textStorage;
+        }
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title =
+    [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes: @{ NSStrokeWidthAttributeName: @3,
+                             NSStrokeColorAttributeName: self.outlineButton.tintColor }
+                   range: NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle: title forState:UIControlStateNormal];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification;
+{
+    [self usePreferredFonts];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)usePreferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection
+{
+    [self.body.textStorage removeAttribute : NSStrokeWidthAttributeName
+                                     range : self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection
+{
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3,
+                                            NSStrokeColorAttributeName : [UIColor blackColor] }
+                                   range:self.body.selectedRange];
+}
+
+
+@end
